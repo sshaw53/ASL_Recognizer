349679dd5b2b6120bfeae6a2a980ff46766237e5
sierrashaw <sierra.shaw@menloschool.org>
2024-04-26 14:33:24 -0700

    Debugged the len(arr) issue and was able to start recording data.

--- asl_recognizer.py
+++ asl_recognizer.py
@@ -38,6 +38,10 @@ class ASL:
         self.letter = ''
 
         self.data = {}
+
+        self.data.clear()
+
+        self.currenttrack_letter = 'a'
     
     def draw_landmarks_on_hand(self, image, detection_result):
         """
@@ -74,9 +78,27 @@ class ASL:
             self.data[landmark_names[index] + "_X"] = []
             self.data[landmark_names[index] + "_Y"] = []
 
-        self.data[landmark_names[index] + "_X"].append(landmark_info.x)
-        self.data[landmark_names[index] + "_Y"].append(landmark_info.y)
+        if landmark_info == None:
+            self.data[landmark_names[index] + "_X"].append(None)
+            self.data[landmark_names[index] + "_Y"].append(None)
+        else:
+            self.data[landmark_names[index] + "_X"].append(landmark_info.x)
+            self.data[landmark_names[index] + "_Y"].append(landmark_info.y)
 
+        # Fixing the varying len of array issue
+        largest_len = 0
+        for key in self.data:
+            if key != "Letter":
+                if len(self.data[key]) > largest_len:
+                    largest_len = len(self.data[key])
+            
+        for key in self.data:
+            if key != 'Letter':
+                if len(self.data[key]) < largest_len:
+                    for i in range (largest_len - len(self.data[key])):
+                        self.data[key].append(None)
+        
+        self.data["Letter"] = self.currenttrack_letter
 
     
     def run(self):
@@ -98,6 +120,9 @@ class ASL:
             # Draw letter onto screen
             cv2.putText(image, str(self.letter), (50, 50), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=BLUE, thickness=2)
 
+            # Draw the rectangle size on the screen
+            cv2.rectangle(image, (400,150), (900,650), BLUE, 4)
+
             # Convert the image to a readable format and find the hands
             to_detect = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
             results = self.detector.detect(to_detect)
@@ -109,22 +134,41 @@ class ASL:
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             cv2.imshow('ASL Recognizer', image)
 
+            key_pressed = cv2.waitKey(15) & 0xFF
+
             # Record the hand locations if the user presses 'p' key
-            if cv2.waitKey(15) == ord('p'):
+            if key_pressed == ord('p'):
+                # landmark_names = ["WRIST", "THUMB_CMC", "THUMB_MCP", "THUMB_IP", "THUMB_TIP", "INDEX_FINGER_MCP", "INDEX_FINGER_PIP", "INDEX_FINGER_DIP",
+                #             "INDEX_FINGER_TIP","MIDDLE_FINGER_MCP", "MIDDLE_FINGER_PIP", "MIDDLE_FINGER_DIP", "MIDDLE_FINGER_TIP", "RING_FINGER_MCP",
+                #             "RING_FINGER_MCP", "RING_FINGER_PIP", "RING_FINGER_DIP", "RING_FINGER_TIP", "PINKY_MCP", "PINKY_PIP", "PINKY_DIP", "PINKY_TIP"]
+                # for i in range (21):
+                #     if landmark_names[i] in results.hand_landmarks:
+                #         self.copy_info(i, results.hand_landmarks[smth][i])
+                #     else:
+                #         self.copy_info(i, None)
+
                 for landmark in results.hand_landmarks:
                     for i in range (21):
                         self.copy_info(i, landmark[i])
+                # for landmark in results.hand_landmarks:
+                #     print(landmark)
                 
-                print(self.data)
-                # where to turn it into a dataframe
-                # USE: data_frame = pd.DataFrame.from_dict(self.data)
-                # clear using self.data.clear()
-
+                #print(self.data)
+            
+            # Changes to the next letter for data capturing
+            if key_pressed == ord('n'):
+                self.currenttrack_letter
+                code = ord(self.currenttrack_letter)
+                self.currenttrack_letter = chr(code + 1)
 
             # Break the loop if the user presses 'q'
-            if cv2.waitKey(15) & 0xFF == ord('q'):
+            if key_pressed == ord('q'):
+                # Turn the dict data into a dataframe
+                data_frame = pd.DataFrame.from_dict(self.data)
+                data_frame.to_csv('data/landmark_locations.csv', index=False, mode='a')
+                #data_frame.to_csv('data/landmark_locations', index=False)
                 break
-                    
+
         self.video.release()
         cv2.destroyAllWindows()
 
